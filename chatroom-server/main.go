47c7c0ea07795caaea1d18044893e611828400c8
Santiago Valle <snvc00@gmail.com>
2021-11-08 11:07:50 -0600

    Add server templates

--- chatroom-server/main.go
+++ chatroom-server/main.go
@@ -89,12 +89,9 @@ func HandleClientConnection(res http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func newChatServer() ChatServer {
-	var host, image string
+func chatServerFromStdin(host *string) ChatServer {
+	var image string
 	scanner := bufio.NewScanner(os.Stdin)
-
-	fmt.Print("Host: ")
-	fmt.Scanln(&host)
 
 	fmt.Print("Name: ")
 	scanner.Scan()
@@ -107,7 +104,47 @@ func newChatServer() ChatServer {
 	fmt.Print("Image: ")
 	fmt.Scanln(&image)
 
-	return ChatServer{Title: serverName, UsersCount: 0, Host: host, Image: image, Description: description}
+	return ChatServer{Title: serverName, UsersCount: 0, Host: *host, Image: image, Description: description}
+}
+
+func chatServerFromTemplate(host *string) ChatServer {
+	templates := make([]ChatServer, 4)
+	var option int
+
+	templates[0] = ChatServer{Title: "Apex Legends", UsersCount: 0, Host: *host, Image: "https://logodownload.org/wp-content/uploads/2019/02/apex-legends-logo-1.png", Description: "Shooter, Multiplayer, Battle Royal, Rankeds available."}
+	templates[1] = ChatServer{Title: "League of Legends", UsersCount: 0, Host: *host, Image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/LoL_icon.svg/2048px-LoL_icon.svg.png", Description: "MOBA, Online, Free to Play, Runs on a Toaster."}
+	templates[2] = ChatServer{Title: "Valorant", UsersCount: 0, Host: *host, Image: "https://studio.cults3d.com/4QqRV9kLYYEuw9ur_X3yjQl1sjk=/516x516/https://files.cults3d.com/uploaders/15024335/illustration-file/a86d53e4-2bd9-4a8f-9550-986686c3131a/gi0mAjIh_400x400.png", Description: "I do not know what to say about this game, there are a lot of tryhards, but remember, DO NOT PEEK."}
+	templates[3] = ChatServer{Title: "Overwatch", UsersCount: 0, Host: *host, Image: "https://upload.wikimedia.org/wikipedia/commons/5/55/Overwatch_circle_logo.svg", Description: "There is a free version of this game, it is called Paladins."}
+
+	fmt.Println("Select your template (default: Apex Legends)")
+	for i, template := range templates {
+		fmt.Print(i, ". ", template.Title, "\n")
+	}
+	fmt.Print("Option: ")
+	fmt.Scanln(&option)
+
+	if option >= len(templates) {
+		option = 0
+	}
+
+	return templates[option]
+}
+
+func newChatServer() ChatServer {
+	var option int
+	var host string
+
+	fmt.Print("1. Use a template\n2. Create new\nOption: ")
+	fmt.Scanln(&option)
+
+	fmt.Print("Host: ")
+	fmt.Scanln(&host)
+
+	if option == 1 {
+		return chatServerFromTemplate(&host)
+	} else {
+		return chatServerFromStdin(&host)
+	}
 }
 
 func main() {
