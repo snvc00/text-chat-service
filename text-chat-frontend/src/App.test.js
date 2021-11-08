479854f90918483b9a904b33399918841c4cf448
Santiago Valle <snvc00@gmail.com>
2021-11-08 10:50:55 -0600

    Add files from cra-template

--- text-chat-frontend/src/App.test.js
+++ text-chat-frontend/src/App.test.js
@@ -3,8 +3,8 @@ import { screen } from '@testing-library/react'
 import { render } from './test-utils'
 import App from './App'
 
-test('renders learn react link', () => {
+test('renders login modal', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn chakra/i)
-  expect(linkElement).toBeInTheDocument()
+  const loginModal = screen.getByText(/Text Chat Service/i)
+  expect(loginModal).toBeInTheDocument()
 })
--- text-chat-frontend/src/test-utils.js
+++ text-chat-frontend/src/test-utils.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { ChakraProvider, theme } from '@chakra-ui/react'
+import { object } from 'prop-types'
 
 const AllProviders = ({ children }) => (
   <ChakraProvider theme={theme}>{children}</ChakraProvider>
 )
+
+AllProviders.propTypes = {
+  children: object
+}
 
 const customRender = (ui, options) =>
   render(ui, { wrapper: AllProviders, ...options })
